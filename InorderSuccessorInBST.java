d7eae4e69cee9188eee535dafe8635a01c5bf127
Kevin <majorgrinch@gmail.com>
2018-02-05 03:43:22 -0600

    add inorder successor in bst java version

---
+++ InorderSuccessorInBST.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null || p == null){
+            return null;
+        }
+        if(p.val >= root.val){
+            return inorderSuccessor(root.right, p);
+        }else{
+            TreeNode left = inorderSuccessor(root.left, p);
+            return left==null ? root : left;
+        }
+    }
+}
