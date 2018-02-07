67520dc44ac27534c131b95d267624f670b1ffe1
Kevin <majorgrinch@gmail.com>
2018-02-07 02:41:18 -0600

    add read n chars given read4 2 java version

---
+++ ReadNCharsGivenRead4_2.java
@@ -0,0 +1,29 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+/**
+ * @param buf Destination buffer
+ * @param n   Maximum number of characters to read
+ * @return    The number of characters read
+ */
+    char[] buffer = new char[4];
+    int head = 0, rear = 0;
+    public int read(char[] buf, int n) {
+        int i = 0;
+        while(i < n){
+            if(head == rear){
+                head = 0;
+                rear = read4(buffer);
+                if(rear == 0){
+                    return i;
+                }
+            }
+
+            while(i < n && head < rear){
+                buf[i++] = buffer[head++];
+            }
+        }
+        return i;
+    }
+}
