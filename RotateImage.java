2785068731f25f0a752544ab237afd5030703948
Kevin <majorgrinch@gmail.com>
2018-02-12 12:20:31 -0600

    add another method to rotate image java version

--- RotateImage.java
+++ RotateImage.java
@@ -15,3 +15,31 @@ class Solution {
         }
     }
 }
+
+public class Solution {
+    /*
+     * @param matrix: a lists of integers
+     * @return: 
+     */
+    public void rotate(int[][] matrix) {
+        // write your code here
+        int n = matrix.length;
+        if(n == 0){
+            return;
+        }
+        for(int i = 0; i < n/2; i++){
+            for(int j = 0; j < n; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-i][j];
+                matrix[n-1-i][j] = tmp;
+            }
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
