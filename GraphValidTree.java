af677d77785a9f207d0cb2ced2e4f36ef5a649d4
Kevin <majorgrinch@gmail.com>
2018-02-26 20:53:59 -0600

    update graph valid tree with bfs approach java version

--- GraphValidTree.java
+++ GraphValidTree.java
@@ -39,3 +39,50 @@ class Solution {
         return true;
     }
 }
+
+/**
+ * BFS version
+ */
+class Solution {
+    Map<Integer, Set<Integer>> createGraph(int n, int[][] edges){
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            graph.put(i, new HashSet<Integer>());
+        }
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        return graph;
+    }
+    public boolean validTree(int n, int[][] edges) {
+        if(n == 1 && edges.length == 0){
+            return true;
+        }
+        if(n-1 != edges.length){
+            return false;
+        }
+        Map<Integer, Set<Integer>> ll_graph = createGraph(n, edges);
+        Queue<Integer> qn = new ArrayDeque<>();
+        boolean[] vis = new boolean[n];
+        qn.offer(edges[0][0]);
+        vis[edges[0][0]] = true;
+        while(!qn.isEmpty()){
+            int curr_n = qn.poll();
+            for(int neighbour : ll_graph.get(curr_n)){
+                if(!vis[neighbour]){
+                    vis[neighbour] = true;
+                    qn.offer(neighbour);
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
