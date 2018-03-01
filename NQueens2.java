8682bb4fdbb2fac1d3029690fa06d1a6ca2c0bed
Kevin <majorgrinch@gmail.com>
2018-02-28 21:22:45 -0600

    add n-queens 2 java version

--- NQueens2.java
+++ NQueens2.java
@@ -1,20 +1,18 @@
 class Solution {
-    List<List<String>> ans;
-    public List<List<String>> solveNQueens(int n) {
-        // write your code here
-        ans = new ArrayList<>();
+    int count;
+    public int totalNQueens(int n) {
+        count = 0;
         if(n == 0){
-            return ans;
+            return count;
         }
         int[] rowArray = new int[n];
         Arrays.fill(rowArray, -1);
         dfs(rowArray, 0, n);
-        return ans;
+        return count;
     }
     void dfs(int[] rowArray,int k, int n){
         if(k == n){
-            List<String> ans_item = drawBoard(rowArray, n);
-            ans.add(ans_item);
+            count++;
             return;
         }
         for(int j = 0; j < n; j++){
@@ -23,19 +21,7 @@ class Solution {
             }
             rowArray[k] = j;
             dfs(rowArray, k+1, n);
-            rowArray[k] = -1;
         }
-    }
-    List<String> drawBoard(int[] rowArray, int n){
-        List<String> board = new ArrayList<>();
-        for(int j = 0; j < n; j++){
-            StringBuilder row = new StringBuilder();
-            for(int i = 0; i  < n; i++){
-                row.append( (i==rowArray[j]) ? 'Q' : '.');
-            }
-            board.add(row.toString());
-        }
-        return board;
     }
     boolean isConflict(int[] rowArray, int rowIndex, int col){
         int n = rowArray.length;
