2b63448bc16bbd43cf2f3fafe186db79ba2b1157
Kevin <majorgrinch@gmail.com>
2018-03-03 03:55:07 -0600

    update word ladder 2 with bidirectional bfs java version

--- WordLadder2.java
+++ WordLadder2.java
@@ -107,10 +107,10 @@ class Solution2 {
                             continue;
                         sc[i] = c;
                         String child = new String(sc);
-                        if(child.equals(end)){
+                        if (child.equals(end)) {
                             childrenMap.get(curr_str).add(end);
                             found = true;
-                        }else if(dict.contains(child) && !found){
+                        } else if (dict.contains(child) && !found) {
                             next_level.add(child);
                             childrenMap.get(curr_str).add(child);
                         }
@@ -123,27 +123,115 @@ class Solution2 {
             curr_level = next_level;
             next_level = temp;
         }
-        for(String key : childrenMap.keySet()){
+        for (String key : childrenMap.keySet()) {
             System.err.println(key + ": " + childrenMap.get(key));
         }
-        if(found){
+        if (found) {
             LinkedList<String> path = new LinkedList<>();
             path.add(start);
             getPath(start, end, childrenMap, path, ans);
         }
         return ans;
     }
-    void getPath(String curr, String end, 
-                HashMap<String, HashSet<String>> childrenMap, LinkedList<String> path, 
-                List<List<String>> ans){
-        if(curr == end){
+
+    public static void getPath(String curr, String end, HashMap<String, HashSet<String>> childrenMap,
+            LinkedList<String> path, List<List<String>> ans) {
+        if (curr == end) {
             ans.add(new ArrayList<String>(path));
             return;
         }
-        if(!childrenMap.containsKey(curr)){
+        if (!childrenMap.containsKey(curr)) {
             return;
         }
-        for(String str : childrenMap.get(curr)){
+        for (String str : childrenMap.get(curr)) {
+            path.add(str);
+            getPath(str, end, childrenMap, path, ans);
+            path.removeLast();
+        }
+    }
+}
+
+/**
+ * Bidirectional BFS
+ */
+class Solution3 {
+    public List<List<String>> findLadders(String start, String end, List<String> worddict) {
+        List<List<String>> ans = new ArrayList<>();
+        HashSet<String> dict = new HashSet<>(worddict);
+        if (!dict.contains(end)) {
+            return ans;
+        }
+        dict.remove(start);
+        dict.remove(end);
+        HashSet<String> qf = new HashSet<>();
+        HashSet<String> qb = new HashSet<>();
+        qf.add(start);
+        qb.add(end);
+        HashMap<String, HashSet<String>> childrenMap = new HashMap<>();
+        int wordL = start.length();
+        boolean found = false;
+        boolean backward = false;
+        while( !qf.isEmpty() && !qb.isEmpty() && !found){
+            if(qf.size() > qb.size()){
+                HashSet<String> temp = qf;
+                qf = qb;
+                qb = temp;
+                backward = !backward;
+            }
+            dict.removeAll(qf);
+            dict.removeAll(qb);
+            HashSet<String> qt = new HashSet<>();
+            for(String curr_str : qf){
+                // childrenMap.putIfAbsent(curr_str, new HashSet<>());
+                char[] sc = curr_str.toCharArray();
+                for(int i = 0; i < wordL; i++){
+                    char ch = sc[i];
+                    for(char c = 'a'; c <= 'z'; c++){
+                        if(c == ch) continue;
+                        sc[i] = c;
+                        String gen_word = new String(sc);
+                        String child = gen_word;
+                        String parent = curr_str;
+                        if(backward){
+                            String temp = parent;
+                            parent = child;
+                            child = temp;
+                        }
+                        if(qb.contains(gen_word)){
+                            found = true;
+                            childrenMap.putIfAbsent(parent, new HashSet<String>());
+                            childrenMap.get(parent).add(child);
+                        }else if(dict.contains(gen_word) && !found){
+                            qt.add(gen_word);
+                            childrenMap.putIfAbsent(parent, new HashSet<String>());
+                            childrenMap.get(parent).add(child);
+                        }
+                    }
+                    sc[i] = ch;
+                }
+            }
+            qf = qt;
+        }
+        for (String key : childrenMap.keySet()) {
+            System.err.println(key + ": " + childrenMap.get(key));
+        }
+        if(found){
+            LinkedList<String> path = new LinkedList<>();
+            path.add(start);
+            getPath(start, end, childrenMap, path, ans);            
+        }
+        return ans;
+    }
+    void getPath(String curr, String end, HashMap<String, HashSet<String>> childrenMap,
+            LinkedList<String> path, List<List<String>> ans) {
+        if (curr.equals(end)) {
+            ans.add(new ArrayList<String>(path));
+            return;
+        }
+        if (!childrenMap.containsKey(curr)) {
+            return;
+        }
+        for (String str : childrenMap.get(curr)) {
             path.add(str);
             getPath(str, end, childrenMap, path, ans);
             path.removeLast();
@@ -154,9 +242,9 @@ class Solution2 {
 public class WordLadder2 {
     public static void main(String[] args) {
         // String[] input_array = { "ted", "tex", "red", "tax", "tad", "den", "rex", "pee" };
-        String[] input_array = {"a", "b", "c", "d"};
-        Solution2 solution = new Solution2();
-        List<List<String>> ans = solution.findLadders("a", "d", Arrays.asList(input_array));
+        String[] input_array = { "a", "b", "c", "d"};
+        Solution3 solution = new Solution3();
+        List<List<String>> ans = solution.findLadders("a", "c", Arrays.asList(input_array));
         for (List<String> l : ans) {
             System.err.println(l);
         }
