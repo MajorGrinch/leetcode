d6815d8c90999ae559d2f8e9a6813d6311a3063d
Kevin <majorgrinch@gmail.com>
2018-03-19 19:41:11 -0500

    add max points on a line java version

--- MaxPointsOnALine.java
+++ MaxPointsOnALine.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.math.BigDecimal;
 import java.util.*;
 
 class Point {
@@ -10,30 +7,6 @@ class Point {
     Point(int a, int b) { x = a; y = b; }
 }
 
-class FunctionPara{
-    // double k;
-    BigDecimal k;
-    double yp;
-    double b;
-    FunctionPara(BigDecimal k, double yp, double b){
-        this.k = k;
-        this.yp = yp;
-        this.b = b;
-    }
-    @Override
-    public boolean equals(Object o){
-        if( !(o instanceof FunctionPara)){
-            return false;
-        }
-        FunctionPara fp = (FunctionPara)o;
-        return k.compareTo(fp.k) == 0 && Math.abs(fp.yp-this.yp) < 1e-16  && Math.abs(fp.b-this.b) < 1e-16;
-    }
-    @Override
-    public int hashCode(){
-        return (int)(1000000 * this.k.doubleValue());
-    }
-}
- 
 class Solution {
     public int maxPoints(Point[] points) {
         if(points == null || points.length == 0){
@@ -42,46 +15,45 @@ class Solution {
         if(points.length == 1){
             return 1;
         }
-        HashMap<FunctionPara, HashSet<Point>> lmap = new HashMap<>();
-        HashSet<FunctionPara> fpset = new HashSet<>();
+        int ans = 0;
         for(int i = 0; i < points.length-1; i++){
+            Map<String, Integer> hmap = new HashMap<>();
+            int sloped = 0, overlapped = 0, vertical = 0;
             for(int j = i+1; j < points.length; j++){
                 int x1 = points[i].x;
                 int y1 = points[i].y;
                 int x2 = points[j].x;
                 int y2 = points[j].y;
-                FunctionPara fp;
                 if(x1 == x2){
-                    fp = new FunctionPara(new BigDecimal(1.0), 0.0, -x1);
-                }else{
-
-                    BigDecimal k = new BigDecimal(y1-y2).divide(new BigDecimal(x1-x2), 20, BigDecimal.ROUND_HALF_UP);
-                    double b = y1 - k.doubleValue()*x1;
-                    fp = new FunctionPara(k, -1.0, b);
+                    if(y1 == y2){
+                        overlapped++;
+                    }else{
+                        vertical++;
+                    }
+                    continue;
                 }
-                lmap.putIfAbsent(fp, new HashSet<>());
-                lmap.get(fp).add(points[i]);
-                lmap.get(fp).add(points[j]);
-                System.out.println(fp.k);
-                // for(FunctionPara f : lmap.keySet()){
-                //     System.out.println(f.k + " " + f.yp + " " + f.b + " " + lmap.get(f));
-                // }
-                // System.out.println("---------------");
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                int gcdN = gcd(dx, dy);
+                dx /= gcdN;
+                dy /= gcdN;
+                String k = dy + "/" + dx;
+                hmap.put(k, hmap.getOrDefault(k, 0) + 1);
+                sloped = Math.max(sloped, hmap.get(k));
             }
+            ans = Math.max(ans, Math.max(sloped, vertical) + overlapped + 1);
         }
-        int count = 0;
-        for(FunctionPara fp : lmap.keySet()){
-            count = Math.max(lmap.get(fp).size(), count);
-            System.out.println(fp.k + " " + fp.yp + " " + fp.b + " " + lmap.get(fp));
-        }
-        return count; 
+        return ans; 
+    }
+    int gcd(int a, int b){
+        return b==0 ? a : gcd(b, a%b);
     }
 }
 public class MaxPointsOnALine{
     public static void main(String[] args) {
         Point[] points = new Point[3];
-        points[1] = new Point(94911151,94911150);
         points[0] = new Point(0, 0);
+        points[1] = new Point(94911151,94911150);
         points[2] = new Point(94911152,94911151);
         int ans = new Solution().maxPoints(points);
         System.out.println(ans);
