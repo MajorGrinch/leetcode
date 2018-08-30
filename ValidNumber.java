33b5eba7c9615832af852b6f04bef21bafd35f31
Kevin <majorgrinch@gmail.com>
2018-08-29 22:45:52 -0500

    update valid number with new version

--- ValidNumber.java
+++ ValidNumber.java
@@ -35,10 +35,36 @@ class Solution {
         }
         return i == len;
     }
+    public boolean isNumber2(String s){
+        s = s.trim() + " ";
+        char[] sc = s.toCharArray();
+        int i = 0, len = sc.length - 1;
+        int nPoints = 0, nDigits = 0;
+        if(sc[i] == '+' || sc[i] == '-') i++;
+        while(Character.isDigit(sc[i])){
+            nDigits++;
+            i++;
+        }
+        if(sc[i] == '.'){
+            i++;
+            while(Character.isDigit(sc[i])){
+                i++;
+                nDigits++;
+            }
+        }
+        if(nDigits == 0) return false;
+        if(sc[i] == 'e'){
+            i++;
+            if(sc[i] == '+' || sc[i] == '-') i++;
+            if(i == len) return false;
+            while(Character.isDigit(sc[i])) i++;
+        }
+        return i == len;
+    }
 }
 public class ValidNumber{
     public static void main(String[] args) {
-        String input = "514.e10";
+        String input = "-.7e10";
         System.out.println(new Solution().isNumber(input));
     }
 }
