d0c7e6e9461efe7bbee4d9c6b2dc3439bafe4ead
ZIYANG WANG <majorgrinch@gmail.com>
2018-09-01 16:44:41 -0500

    update longest substring with at most 2 with general method

--- LongestSubstrWithAtMost2.java
+++ LongestSubstrWithAtMost2.java
@@ -1,7 +1,7 @@
 public class LongestSubstrWithAtMost2{
     public static void main(String[] args) {
-        String input = "caabbc";
-        System.out.println(new Solution().lengthOfLongestSubstringTwoDistinct(input));
+        String input = "eceba";
+        System.out.println(new Solution().lengthOfLongestSubstringTwoDistinct2(input));
     }
 }
 
@@ -17,5 +17,20 @@ class Solution {
             j = k - 1;
         }
         return Math.max(s.length() - i, maxL);
+    }
+    public int lengthOfLongestSubstringTwoDistinct2(String s){
+        int i = 0, numDistinct = 0, maxL = 0;
+        int[] count = new int[256];
+        for(int j = 0; j < s.length(); j++){
+            if(count[s.charAt(j)] == 0) numDistinct++;
+            count[s.charAt(j)]++;
+            while(numDistinct > 2){
+                count[s.charAt(i)]--;
+                if(count[s.charAt(i)] == 0) numDistinct--;
+                i++;
+            }
+            maxL = Math.max(maxL, j - i + 1);
+        }
+        return maxL;
     }
 }
