213675d210b671680a0061fc7e3ac99560e88677
ZIYANG WANG <majorgrinch@gmail.com>
2018-09-05 21:37:25 -0500

    update plus one with clean code java version

--- PlusOne.java
+++ PlusOne.java
@@ -18,3 +18,19 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        digits[digits.length-1]++;
+        for(int i = digits.length-1; i > 0; i--){
+            digits[i-1] += digits[i] / 10;
+            digits[i] %= 10;
+        }
+        if(digits[0] <= 9) return digits;
+        int[] newDigits = new int[digits.length+1];
+        newDigits[0] = digits[0] / 10;
+        digits[0] %= 10;
+        for(int i = 0; i < digits.length; i++) newDigits[i+1] = digits[i];
+        return newDigits;
+    }
+}
