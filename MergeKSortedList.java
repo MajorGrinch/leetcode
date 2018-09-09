78b33e68ad9526fae9459ed77efad20cd271e46e
Kevin <majorgrinch@gmail.com>
2018-09-09 16:02:12 -0500

    update merge k sorted list with merge 2 approach java version

--- MergeKSortedList.java
+++ MergeKSortedList.java
@@ -34,3 +34,35 @@ public class ListNode {
     ListNode next;
     ListNode(int x) { val = x; }
 }
+class Solution2{
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        return mergeKLists(lists, 0, lists.length-1);
+    }
+    private ListNode mergeKLists(ListNode[] lists, int start, int end){
+        if(start == end) return lists[start];
+        else if(start < end){
+            int mid = start + (start-end)/2;
+            ListNode left = mergeKLists(lists, start, mid);
+            ListNode right = mergeKLists(lists, mid+1, end);
+            return merge(left, right);
+        }else return null;
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode l = new ListNode(0);
+        ListNode p = l;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                p.next = l1;
+                l1 = l1.next;
+            }else{
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if(l1 != null) p.next = l1;
+        if(l2 != null) p.next = l2;
+        return l.next;
+    }
+}
