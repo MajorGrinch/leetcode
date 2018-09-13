ff12c01ae79cc1c64d15c649d7e810286e2e3dc8
ZIYANG WANG <majorgrinch@gmail.com>
2018-09-12 22:53:29 -0500

    update maximum depth of binary tree with new approach java version

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -22,3 +22,9 @@ class Solution {
         return d;
     }
 }
+class Solution2 {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
