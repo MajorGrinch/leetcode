2e248538a65ac5d43e2dbc62ee691065dc839118
Kevin <majorgrinch@gmail.com>
2018-09-19 15:30:46 -0500

    update unique path with bottom-up dp approach java version

--- UniquePath.java
+++ UniquePath.java
@@ -16,3 +16,15 @@ class Solution {
         return mat[row][col+1] + mat[row+1][col];
     }
 }
+
+class Solution2{
+    public int uniquePaths(int m, int n){
+        int[][] mat = new int[m+1][n+1];
+        mat[m-1][n] = 1;
+        for(int r = m-1; r >= 0; r--)
+            for(int c = n-1; c >= 0; c--){
+                mat[r][c] = mat[r+1][c] + mat[r][c+1];
+            }
+        return mat[0][0];
+    }
+}
