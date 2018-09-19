0533ef326257258b7741fe7e40069e53ae5e5861
ZIYANG WANG <majorgrinch@gmail.com>
2018-09-19 11:54:22 -0500

    review decode ways

--- DecodeWays.java
+++ DecodeWays.java
@@ -21,3 +21,19 @@ class Solution {
         return f[len];
     }
 }
+class Solution2 {
+    public int numDecodings(String s){
+        if(s.length() == 0) return 0;
+        char[] sc = s.toCharArray();
+        int[] f = new int[s.length() + 1];
+        f[0] = 1;
+        for(int i = 1; i < f.length; i++){
+            if(sc[i-1] != '0') f[i] += f[i-1];
+            if(i > 1){
+                int tmp = 10 * (sc[i-2] - '0') + sc[i-1] - '0';
+                if(tmp >= 10 && tmp <= 26) f[i] += f[i-2];
+            }
+        }
+        return f[s.length()];
+    }
+}
