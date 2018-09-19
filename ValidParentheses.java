07f1adad8a9921bba0234166a6c713141054e6a8
ZIYANG WANG <majorgrinch@gmail.com>
2018-09-18 23:08:27 -0500

    fix last commit

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -72,7 +72,11 @@ class Solution3{
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()){
-            
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            else if(stack.isEmpty() || stack.pop() != c) return false;
         }
+        return stack.isEmpty();
     }
 }
