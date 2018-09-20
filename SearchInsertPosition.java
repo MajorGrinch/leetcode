d745aa33c546ac465a5c8d3787c7a61863cdea20
Kevin <majorgrinch@gmail.com>
2018-09-19 20:25:25 -0500

    update search insert position with binary search approach java version

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -7,3 +7,28 @@ class Solution {
         return nums.length;
     }
 }
+
+class Solution2{
+    public int searchInsert(int[] nums, int target){
+        return binSearch(nums, 0, nums.length-1, target);
+    }
+    private int binSearch(int[] nums, int lo, int hi, int target){
+        if(nums[lo] > target) return lo;
+        if(nums[hi] < target) return hi + 1;
+        if(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) return binSearch(nums, mid+1, hi, target);
+            else return binSearch(nums, lo, mid-1, target);
+        }
+        return -1;
+    }
+}
+
+public class SearchInsertPosition{
+    public static void main(String[] args) {
+        int[] test = {1,3,5,6};
+        int ans = new Solution2().searchInsert(test, 7);
+        System.out.println(ans);
+    }
+}
