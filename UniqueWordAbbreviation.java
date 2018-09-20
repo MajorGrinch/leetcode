f60d5a67879132d2809fe7e11486a22a92af3a78
Kevin <majorgrinch@gmail.com>
2018-09-19 22:18:36 -0500

    review unique word abbreviation

--- UniqueWordAbbreviation.java
+++ UniqueWordAbbreviation.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 class ValidWordAbbr {
     Map<String, Integer> cmap = new HashMap<>();
     Map<String, Integer> abmap = new HashMap<>();
@@ -22,8 +24,39 @@ class ValidWordAbbr {
     }
 }
 
-/**
- * Your ValidWordAbbr object will be instantiated and called as such:
- * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
- * boolean param_1 = obj.isUnique(word);
- */
+class ValidWordAbbr2 {
+    HashMap<String, Integer> count_word = new HashMap<>();
+    HashMap<String, Integer> count_abbr = new HashMap<>();
+    public ValidWordAbbr2(String[] dictionary) {
+        for(String word : dictionary){
+            count_word.put(word, count_word.getOrDefault(word, 0)+1);
+            String abbr = getAbbr(word);
+            count_abbr.put(abbr, count_abbr.getOrDefault(abbr, 0)+1);
+        }
+    }
+    
+    public boolean isUnique(String word) {
+        String abbr = getAbbr(word);
+        return count_word.getOrDefault(word, 0) == count_abbr.getOrDefault(abbr, 0);
+    }
+    public String getAbbr(String s){
+        if(s.length() < 3) return s;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        sb.append(s.length() - 2);
+        sb.append(s.charAt(s.length()-1));
+        return sb.toString();
+    }
+}
+
+public class UniqueWordAbbreviation{
+    public static void main(String[] args) {
+        String[] dict = {"deer","door","cake","card"};
+        ValidWordAbbr2 vwa = new ValidWordAbbr2(dict);
+        String[] test = {"dear", "cart", "cane", "make"};
+        for(String t : test){
+            System.out.println(vwa.getAbbr(t));
+            System.out.println(vwa.isUnique(t));
+        }
+    }
+}
