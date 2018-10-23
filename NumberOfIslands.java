995e28910f9bc0f154f1b8cadf234c2f4d6ec315
kevin <majorgrinch@gmail.com>
2018-10-23 14:50:35 -0500

    update number of islands with dfs approach java version

--- NumberOfIslands.java
+++ NumberOfIslands.java
@@ -41,3 +41,27 @@ class Solution {
         }
     }
 }
+class Solution2{
+    void dfs(char[][] grid, int x, int y, int row, int col){
+        if(x < 0 || x >= row || y < 0 || y >= col || grid[x][y] == '0') return;
+
+        grid[x][y] = '0';
+        dfs(grid, x+1, y, row, col);
+        dfs(grid, x-1, y, row, col);
+        dfs(grid, x, y-1, row, col);
+        dfs(grid, x, y+1, row, col);
+    }
+    public int numIslands(char[][] grid){
+        int row = grid.length;
+        if(row == 0) return 0;
+        int col = grid[0].length;
+        int count = 0;
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++)
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j, row, col);
+                }
+        return count;
+    }
+}
