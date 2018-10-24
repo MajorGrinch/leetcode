19a8eb58fd6d4727403e12450f097f40957c704e
kevin <majorgrinch@gmail.com>
2018-10-24 18:22:34 -0500

    add trapping rain water dp approach java version

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -15,3 +15,24 @@ class Solution {
         return ans;
     }
 }
+
+class Solution2{
+    public int trap(int[] height){
+        int ans = 0;
+        int len = height.length;
+        int[] maxl = new int[len];
+        int[] maxr = new int[len];
+        maxl[0] = height[0];
+        maxr[len-1] = height[len-1];
+        for(int i = 1; i < len; i++){
+            maxl[i] = Math.max(maxl[i-1], height[i]);
+        }
+        for(int i = len - 2; i >= 0; i--){
+            maxr[i] = Math.max(maxr[i+1], height[i]);
+        }
+        for(int i = 1; i <= len - 2; i++){
+            ans += Math.min(maxl[i], maxr[i]) - height[i];
+        }
+        return ans;
+    }
+}
