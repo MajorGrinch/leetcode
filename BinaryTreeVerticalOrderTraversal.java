962d982f95149441dffbf6790c51a07eb05595e0
kevin <majorgrinch@gmail.com>
2018-10-23 23:28:18 -0500

    review bin tree vertical order with optimization java version

--- BinaryTreeVerticalOrderTraversal.java
+++ BinaryTreeVerticalOrderTraversal.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -37,4 +42,43 @@ class Solution {
         }
         return ans;
     }
+}
+class Solution2{
+    Map<Integer, List<Integer>> colmap;
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        int minl = Integer.MAX_VALUE, maxl = Integer.MIN_VALUE;
+        colmap = new HashMap<>();
+        Queue<TreeNode> qn = new LinkedList<>();
+        Queue<Integer> qc = new LinkedList<>();
+        qn.offer(root);
+        qc.offer(0);
+        while(!qn.isEmpty()){
+            int col = qc.poll();
+            TreeNode curr = qn.poll();
+            minl = Math.min(col, minl);
+            maxl = Math.max(col, maxl);
+            colmap.putIfAbsent(col, new ArrayList<>());
+            colmap.get(col).add(curr.val);
+            if(curr.left != null){
+                qn.offer(curr.left);
+                qc.offer(col-1);
+            }
+            if(curr.right != null){
+                qn.offer(curr.right);
+                qc.offer(col+1);
+            }
+        }
+        for(int i = minl; i <= maxl; i++){
+            ans.add(colmap.get(i));
+        }
+
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
