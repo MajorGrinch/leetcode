be59ccf5f420c8be6eb8c999bab429bc35dc0fce
kevin <majorgrinch@gmail.com>
2018-10-24 23:30:42 -0500

    update longest valid parenthesis stack approach java

--- LongestValidParenthesis.java
+++ LongestValidParenthesis.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 class Solution {
     public int longestValidParentheses(String s) {
@@ -15,6 +16,24 @@ class Solution {
                     dp[i] = dp[i-1] + (i - dp[i-1] >= 2 ? dp[i-dp[i-1]-2] : 0) + 2;
                 }
                 ans = Math.max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution2{
+    public int longestValidParentheses(String s){
+        int ans = 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()) st.push(i);
+                else ans = Math.max(ans, i - st.peek());
             }
         }
         return ans;
