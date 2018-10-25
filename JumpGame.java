b5d5ae4d3129a47e1ae86f425e97515380313598
kevin <majorgrinch@gmail.com>
2018-10-25 00:49:36 -0500

    update jump game with greedy algo java version

--- JumpGame.java
+++ JumpGame.java
@@ -14,6 +14,18 @@ class Solution {
 
 }
 
+class Solution2{
+    public boolean canJump(int[] nums){
+        int lastpos = nums.length-1;
+        for(int i = nums.length-2; i >= 0; i--){
+            if(nums[i] + i >= lastpos){
+                lastpos = i;
+            }
+        }
+        return lastpos == 0;
+    }
+}
+
 public class JumpGame{
     public static void main(String[] args) {
         int[] input = {8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
