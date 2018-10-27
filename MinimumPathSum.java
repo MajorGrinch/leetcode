9740d93075f3adc45885e655b07ae61b6d0e575a
kevin <majorgrinch@gmail.com>
2018-10-27 00:21:17 -0500

    add minimum path sum 2d dp java

---
+++ MinimumPathSum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid.length == 0){
+            return 0;
+        }
+        int[][] dp = new int[grid.length][grid[0].length];
+        int m = dp.length, n = dp[0].length;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(i == m-1 && j != n-1){
+                    dp[i][j] = grid[i][j] + dp[i][j+1];
+                }else if(j == n-1 && i != m-1){
+                    dp[i][j] = grid[i][j] + dp[i+1][j];
+                }else if(i == m-1 && j == n-1){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    dp[i][j] = grid[i][j] + Math.min(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
