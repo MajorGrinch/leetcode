7098f4750757eeb5e437e0448ee518f8c3dd283d
kevin <majorgrinch@gmail.com>
2018-10-31 23:24:19 -0500

    add maximal rectangle with dp approach java

---
+++ MaximalRectangle.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0)
+            return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] height = new int[n];
+        for(int i = 0; i < n; i++){
+            left[i] = 0;
+            right[i] = n;
+            height[i] = 0;
+        }
+        // Arrays.fill(right, n);
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            int curr_left = 0, curr_right = n;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], curr_left);
+                } else {
+                    left[j] = 0;
+                    curr_left = j + 1;
+                }
+            }
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], curr_right);
+                } else {
+                    right[j] = n;
+                    curr_right = j;
+                }
+            }
+            for(int j = 0; j < n; j++){
+                ans = Math.max(ans, (right[j] - left[j])*height[j]);
+            }
+        }
+        return ans;
+    }
+}
