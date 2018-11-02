c8e33679d8c4e12e1feeb5059073df4e8d4588e4
kevin <majorgrinch@gmail.com>
2018-11-02 18:58:56 -0500

    update linked list cycle fast approach java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -22,3 +22,17 @@ class Solution {
         return false;
     }
 }
+
+class Solution{
+    public boolean hasCycle(ListNode head){
+        if(head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow != fast){
+            if(fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
