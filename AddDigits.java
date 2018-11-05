42ed485c972e7844ff6806500408a95ce9b5af69
kevin <majorgrinch@gmail.com>
2018-11-05 01:18:21 -0600

    review add digits with smart approach java

--- AddDigits.java
+++ AddDigits.java
@@ -16,3 +16,14 @@ class Solution {
         return ans;    
     }
 }
+
+class Solution2{
+    public int addDigits(int num){
+        if(num == 0) return 0;
+        if(num % 9 == 0){
+            return 9;
+        }else{
+            return num % 9;
+        }
+    }
+}
