91da76aabd8f51770f55294419ba0ec5ccd9ff2f
kevin <majorgrinch@gmail.com>
2018-11-05 14:45:51 -0600

    add majority element java

---
+++ MajorityElement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt = 0;
+        int candidate = 0;
+        for(int num : nums){
+            if(cnt == 0){
+                candidate = num;
+            }
+            cnt += (num==candidate ? 1: -1);
+        }
+        return candidate;
+    }
+}
