c2d1fb24504629542ebddddd466e0b155acde96e
kevin <majorgrinch@gmail.com>
2018-11-07 00:35:44 -0600

    update kth largest ele in array java

--- KthLargestElementInArray.java
+++ KthLargestElementInArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         k = nums.length - k;
@@ -37,5 +39,52 @@ class Solution {
         }
         exch(nums, lo, j);
         return j;
+    }
+}
+
+class Solution2{
+    public int findKthLargest(int[] nums, int k) {
+        return partition(nums, 0, nums.length-1, nums.length-k);
+    }
+    private void exch(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private int partition(int[] nums, int lo, int hi, int k) {
+        if(lo >= hi){
+            return nums[lo];
+        }
+        int i = lo, j = hi + 1;
+        int v = nums[lo];
+        while (true) {
+            while (nums[++i] < v) {
+                if (i == hi)
+                    break;
+            }
+            while (nums[--j] > v) {
+                if (j == lo)
+                    break;
+            }
+            if (i >= j)
+                break;
+            exch(nums, i, j);
+        }
+        exch(nums, lo, j);
+        if(j < k){
+            partition(nums, j+1, hi, k);
+        }else if(j > k){
+            partition(nums, lo, j-1, k);
+        }
+        return nums[k];
+    }
+}
+
+public class KthLargestElementInArray{
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        // int[] nums = {11,11,11};
+        int ans = new Solution2().findKthLargest(nums, 4);
+        System.out.println(ans);
     }
 }
