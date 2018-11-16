316e4e88ffc3c577fd2e9217a4bcc77a339bc692
kevin <majorgrinch@gmail.com>
2018-11-16 15:49:01 -0600

    add hourse robber 3 with slow approach java

---
+++ HouseRobber3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(TreeNode root) {
+        if(root == null) return 0;
+        int val = 0;
+        if(root.left != null){
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+        if(root.right != null){
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+        return Math.max(root.val + val, rob(root.left)+rob(root.right)); 
+    }
+}
