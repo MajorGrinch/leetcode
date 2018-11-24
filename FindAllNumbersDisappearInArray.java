02f113c54af2fdf64401d7057ed52645960d033b
kevin <majorgrinch@gmail.com>
2018-11-24 14:38:57 -0600

    add find all numbers disappear in array o(n) java

---
+++ FindAllNumbersDisappearInArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            int val = nums[idx];
+            nums[idx] = val < 0 ? val : -val;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+        return ans;
+    }
+}
