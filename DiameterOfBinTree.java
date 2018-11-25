4031c24f13cd6230e6471f2cd4ac8331cb615689
kevin <majorgrinch@gmail.com>
2018-11-24 21:31:55 -0600

    update diameter of binary tree java

--- DiameterOfBinTree.java
+++ DiameterOfBinTree.java
@@ -31,3 +31,20 @@ class Solution {
     }
 
 }
+
+class Solution{
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root){
+        if(root == null) return 0;
+        ans = 0;
+        dfs(root);
+        return ans - 1;
+    }
+    private int dfs(TreeNode node){
+        if(node == null) return 0;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        ans = Math.max(ans, left+right+1);
+        return Math.max(left, right) + 1;
+    }
+}
