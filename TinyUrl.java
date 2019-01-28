c5b79a96bf0a06b2c396368f08080e2ebe61b54f
kevin <majorgrinch@gmail.com>
2019-01-28 14:26:55 -0600

    update tiny url with advanced app java

--- TinyUrl.java
+++ TinyUrl.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 class Codec {
     Map<Integer, String> urlMap = new HashMap<>();
@@ -19,3 +20,30 @@ class Codec {
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(url));
+
+class Codec2{
+    Map<String, String> urlMap = new HashMap<>();
+    String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    Random rand = new Random();
+    String key = getKey();
+
+    public String getKey(){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 6; i++){
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        return sb.toString();
+    }
+    public String encode(String longUrl) {
+        while(urlMap.containsKey(key)){
+            key = getKey();
+        }
+        urlMap.put(key, longUrl);
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return urlMap.get(shortUrl.substring(19));
+    }
+}
