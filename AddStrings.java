ed78cc35874d66a17013f107e0799e098463ac78
kevin <majorgrinch@gmail.com>
2019-01-28 13:58:48 -0600

    update add string with fast app java

--- AddStrings.java
+++ AddStrings.java
@@ -22,4 +22,27 @@ class Solution {
         }
         return sb.toString();
     }
+
+    class Solution2 {
+        public String addStrings(String num1, String num2) {
+            if(num1.length() < num2.length()){
+                return addStrings(num2, num1);
+            }
+            char[] n1 = num1.toCharArray();
+            char[] n2 = num2.toCharArray();
+            // int[] ans = new int[n1.length];
+            int carry = 0;
+            StringBuilder sb = new StringBuilder();
+            for(int i = n1.length-1, j = n2.length-1; i >= 0; i--, j--){
+                int sum = 0;
+                sum += i >= 0 ? n1[i] - '0' : 0;
+                sum += j >= 0 ? n2[j] - '0' : 0;
+                sum += carry;
+                sb.append(sum % 10);
+                carry = sum / 10;
+            }
+            if(carry != 0) sb.append(carry);
+            return sb.reverse().toString();
+        }
+    }
 }
