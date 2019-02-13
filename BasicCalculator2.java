95dab2fe99fc8762794396babeafeff9d4f105d6
kevin <majorgrinch@gmail.com>
2019-02-13 15:34:38 -0600

    add basic calculator 2 with paren java

--- BasicCalculator2.java
+++ BasicCalculator2.java
@@ -2,13 +2,15 @@ import java.util.Stack;
 
 class Solution {
     public int calculate(String s) {
-        s = s.trim();
+        s = s.trim() ;
         Stack<Integer> operandStack = new Stack<>();
         Stack<Character> operatorStack = new Stack<>();
 
         int currNum = 0;
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
+            // System.out.println("current c : " + c);
+            if(c == ' ') continue;
             if(c >= '0' && c <= '9'){
                 currNum = currNum * 10 + c - '0';
             }else{
@@ -17,8 +19,9 @@ class Solution {
                     continue;
                 }
                 if(s.charAt(i-1) != ')'){
-                    // if no ) on the left, there must be a num
+                    // if no ) on the left, there must be a num needed to be push
                     operandStack.push(currNum);
+                    currNum = 0;
                 }
                 if(c == ')'){
                     while(!operatorStack.isEmpty()){
@@ -27,33 +30,46 @@ class Solution {
                         else{
                             int num2 = operandStack.pop();
                             int num1 = operandStack.pop();
+                            char nxtOp = operatorStack.isEmpty() ? ' ' : operatorStack.peek();
+                            if(nxtOp == '-'){
+                                if(op == '+') op = '-';
+                                else if(op == '-') op = '+';
+                            }
                             operandStack.push(cal(num1, num2, op));
                         }
                     }
+                    continue;
                 }
                 if(!operatorStack.isEmpty() && getPri(c) <= getPri(operatorStack.peek())){
                     int num2 = operandStack.pop();
                     int num1 = operandStack.pop();
                     char op = operatorStack.pop();
+                    char nxtOp = operatorStack.isEmpty() ? ' ' : operatorStack.peek();
+                    if(nxtOp == '-'){
+                        if(op == '+') op = '-';
+                        else if(op == '-') op = '+';
+                    }
                     operandStack.push(cal(num1, num2, op));
-                }else{
-                    operandStack.push(currNum);
-                    operatorStack.push(c);
-                    currNum = 0;
                 }
+                operatorStack.push(c);
             }
+            // printStack(operandStack, operatorStack);
 
         }
         if(s.charAt(s.length()-1) != ')') operandStack.push(currNum);
-        printStack(operandStack, operatorStack);
+        // printStack(operandStack, operatorStack);
         while(!operatorStack.isEmpty()){
             int num2 = operandStack.pop();
             int num1 = operandStack.pop();
             char op = operatorStack.pop();
+            char nxtOp = operatorStack.isEmpty() ? ' ' : operatorStack.peek();
+            if(nxtOp == '-'){
+                if(op == '+') op = '-';
+                else if(op == '-') op = '+';
+            }
             operandStack.push(cal(num1, num2, op));
         }
         return operandStack.peek();
-        
     }
 
     private void printStack(Stack<Integer> operandStack, Stack<Character> operatorStack){
@@ -100,6 +116,9 @@ class Solution {
             case '/':
                 pri = 2;
                 break;
+            case '(':
+                pri = 0;
+                break;
         }
         return pri;
     }
@@ -107,7 +126,8 @@ class Solution {
 
 public class BasicCalculator2{
     public static void main(String[] args) {
-        String input = "(3+2)*2";
+        // String input = "((3+2)*2+5 * (10-7))*5 +(3+9)*(1+1)";
+        String input = "282-1*2*13-30-2*2*2/2-95/5*2+55+804+3024";
         System.out.println(new Solution().calculate(input));
     }
 }
