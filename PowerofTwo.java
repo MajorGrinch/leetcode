4388e8b3282d7e311170fa33c3f0ce93d01b1fc6
kevin <majorgrinch@gmail.com>
2019-02-20 13:55:40 -0600

    update power of two n&n-1 trick java

--- PowerofTwo.java
+++ PowerofTwo.java
@@ -11,6 +11,12 @@ class Solution {
     }
 }
 
+class Solution2{
+    public boolean isPowerOfTwo(int n){
+        return n > 0 && (n&n-1) == 0;
+    }
+}
+
 public class PowerofTwo{
     public static void main(String[] args) {
         int n = 1073741825;
