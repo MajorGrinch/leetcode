3d0a78e9bfaa347cc8279a2dec44a3eb250c4585
kevin <majorgrinch@gmail.com>
2019-02-24 11:15:27 -0600

    update rm dup letters stack app java

--- RemoveDuplicateLetters.java
+++ RemoveDuplicateLetters.java
@@ -36,9 +36,38 @@ class Solution {
     }
 }
 
+class Solution2{
+    public String removeDuplicateLetters(String s){
+        if(s.length() == 0) return s;
+        int[] cnt = new int[26];
+        char[] sc = s.toCharArray();
+        for(char c : sc){
+            cnt[c-'a']++;
+        }
+        Stack<Character> ansStack = new Stack<>();
+        boolean[] used = new boolean[26];
+        for(char c : sc){
+            int idx = c - 'a';
+            cnt[idx]--;
+            if(used[idx]) continue;
+
+            while(!ansStack.isEmpty() && ansStack.peek() > c && cnt[ansStack.peek()-'a'] > 0){
+                used[ansStack.pop()-'a'] = false;
+            }
+            ansStack.push(c);
+            used[c-'a'] = true;
+        }
+        StringBuffer res = new StringBuffer();
+        for(char c : ansStack){
+            res.append(c);
+        }
+        return res.toString();
+    }
+}
+
 public class RemoveDuplicateLetters{
     public static void main(String[] args) {
         String input = "cbacdcbc";
-        System.out.println(new Solution().removeDuplicateLetters(input));
+        System.out.println(new Solution2().removeDuplicateLetters(input));
     }
 }
