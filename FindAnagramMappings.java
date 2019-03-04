5f99f8400e45d1aa2df62aa6c3866d9c1f9d7dac
kevin <majorgrinch@gmail.com>
2019-03-04 15:36:43 -0600

    update find anagram mapping with duplicate mapping java

--- FindAnagramMappings.java
+++ FindAnagramMappings.java
@@ -15,3 +15,17 @@ class Solution {
         return mapping;
     }
 }
+
+class Solution2 {
+    public int[] anagramMappings(int[] A, int[] B) {
+        Map<Integer, Integer> idxMap = new HashMap<>();
+        for(int i = 0; i < B.length; i++){
+            idxMap.put(B[i], i);
+        }
+        int[] mapping = new int[A.length];
+        for(int i = 0; i < A.length; i++){
+            mapping[i] = idxMap.get(A[i]);
+        }
+        return mapping;
+    }
+}
