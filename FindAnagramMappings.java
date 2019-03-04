96e46c17a638916983e6a60fa08eea56e80eb2ca
kevin <majorgrinch@gmail.com>
2019-03-04 15:28:58 -0600

    add find anagram mappings with distinct mapping java

---
+++ FindAnagramMappings.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        Map<Integer, List<Integer>> idxMap = new HashMap<>();
+        for(int i = 0; i < B.length; i++){
+            if(!idxMap.containsKey(B[i]))
+                idxMap.put(B[i], new ArrayList<Integer>());
+            idxMap.get(B[i]).add(i);
+        }
+        int[] mapping = new int[A.length];
+        for(int i = 0; i < A.length; i++){
+            mapping[i] = idxMap.get(A[i]).get(0);
+            idxMap.get(A[i]).remove(0);
+        }
+        return mapping;
+    }
+}
