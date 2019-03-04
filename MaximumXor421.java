6f7debfe50720c3d45ceec251a7fab3b6065e016
kevin <majorgrinch@gmail.com>
2019-03-03 23:13:55 -0600

    update 421 maximum xor 421 with prefix trie

--- MaximumXor421.java
+++ MaximumXor421.java
@@ -20,3 +20,57 @@ class Solution {
         return ans;
     }
 }
+
+class Solution2{
+    public int findMaximumXOR(int[] nums){
+        TrieNode prefixTrie = new TrieNode();
+        for(int num : nums){
+            add(prefixTrie, num);
+        }
+        int ans = 0;
+        for(int num : nums){
+            int another = findPair(prefixTrie, num);
+            ans = Math.max(ans, another ^ num);
+        }
+        return ans;
+    }
+
+    private void add(TrieNode root, int num){
+        for(int i = 31; i >= 0; i--){
+            int currBit = (num >> i) & 1;
+            if(currBit == 0){
+                if(root.left == null) root.left = new TrieNode();
+                root = root.left;
+            }else{
+                if(root.right == null) root.right = new TrieNode();
+                root = root.right;
+            }
+        }
+    }
+
+    private int findPair(TrieNode root, int num){
+        int res = 0;
+        for(int i = 31; i >= 0; i--){
+            int currBit = (num >> i) & 1;
+            if(currBit == 0){
+                if(root.right != null){
+                    root = root.right;
+                    res += 1 << i;
+                }else{
+                    root = root.left;
+                }
+            }else{
+                if(root.left != null) root = root.left;
+                else{
+                    root = root.right;
+                    res += 1 << i;
+                }
+            }
+        }
+        return res;
+    }
+    class TrieNode{
+        TrieNode left;
+        TrieNode right;
+    }
+}
