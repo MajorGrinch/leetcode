50b0aa23ca9d610b6c94627ae8f22fb8b105d18b
kevin <majorgrinch@gmail.com>
2019-03-05 21:39:22 -0600

    update trapping rain water with o1 space app java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -57,3 +57,24 @@ class Solution3{
         return ans;
     }
 }
+
+class Solution4{
+    public int trap(int[] height){
+        if(height == null || height.length < 3) return 0;
+        int ans = 0;
+        int l = 0, r = height.length-1;
+        int lmax = 0, rmax = 0;
+        while(l < r){
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            if(lmax < rmax){
+                ans += lmax - height[l];
+                l++;
+            }else{
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
