b52f9452afa18a60eb34d36a04fca759abc2f74f
ZIYANG WANG <majorgrinch@gmail.com>
2019-03-05 20:00:27 -0600

    update first missing positive with put number in right place app java

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -8,3 +8,24 @@ class Solution {
         }
     }
 }
+
+class Solution2{
+    public int firstMissingPositive(int[] nums){
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]){
+                swap(nums, i, nums[i]-1);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] != i + 1) return i + 1;
+        }
+        return n+1;
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int tmp =  nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
