0c95e20e78911813163ad8bfc96cc125e77711d1
kevin <majorgrinch@gmail.com>
2019-03-17 15:55:08 -0500

    update 235 lca of bst with fast app java

--- LowestCommonAncestorOfABinarySearchTree.java
+++ LowestCommonAncestorOfABinarySearchTree.java
@@ -29,3 +29,17 @@ class Solution {
         return null;
     }
 }
+
+class Solution2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(q.val < p.val) return lowestCommonAncestor(root, q, p);
+        return LCA(root, p, q);
+    }
+
+    private TreeNode LCA(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null) return root;
+        if(root.val >= p.val && root.val <= q.val) return root;
+        else if(root.val <= p.val && root.val <= q.val) return LCA(root.right, p, q);
+        else return LCA(root.left, p, q);
+    }
+}
